832280add416577a9090abd15921c21308cc7230
Jason Lewis <jason.lewis1991@gmail.com>
2013-12-08 12:03:13 +1100

    Updating file resource tests.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- tests/FileResourceTest.php
+++ tests/FileResourceTest.php
@@ -6,17 +6,27 @@ use JasonLewis\ResourceWatcher\Resource\FileResource;
 
 class FileResourceTest extends PHPUnit_Framework_TestCase {
 
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+
 	public function testCanGetResourceAttributes()
 	{
 		$files = m::mock('Illuminate\Filesystem\Filesystem');
 		$files->shouldReceive('exists')->once()->andReturn(true);
 		$files->shouldReceive('lastModified')->twice()->andReturn($modified = time());
-		$resource = new FileResource(__DIR__.'/mock.file', $files);
-		$this->assertEquals(md5(__DIR__.'/mock.file'), $resource->getKey());
-		$this->assertEquals(__DIR__.'/mock.file', $resource->getPath());
+
+		$resource = new FileResource(new SplFileInfo(__FILE__), $files);
+
+		$this->assertEquals(md5(__FILE__), $resource->getKey());
+		$this->assertEquals(__FILE__, $resource->getPath());
 		$this->assertEquals($modified, $resource->getLastModified());
 		$this->assertFalse($resource->isModified());
 	}
+
 
 	public function testDetectingOfResourceCreated()
 	{
@@ -24,23 +34,28 @@ class FileResourceTest extends PHPUnit_Framework_TestCase {
 		$files->shouldReceive('exists')->once()->andReturn(false);
 		$files->shouldReceive('exists')->once()->andReturn(true);
 		$files->shouldReceive('lastModified')->once()->andReturn(time());
-		$resource = new FileResource(__DIR__.'/mock.file', $files);
+
+		$resource = new FileResource(new SplFileInfo(__FILE__), $files);
+
 		$events = $resource->detectChanges();
 		$this->assertInstanceOf('JasonLewis\ResourceWatcher\Event', $event = array_pop($events));
 		$this->assertEquals(Event::RESOURCE_CREATED, $event->getCode());
 	}
 
+
 	public function testDetectingOfResourceModified()
 	{
 		$files = m::mock('Illuminate\Filesystem\Filesystem');
 		$files->shouldReceive('exists')->twice()->andReturn(true);
-		$files->shouldReceive('lastModified')->once()->andReturn(time());
-		$files->shouldReceive('lastModified')->once()->andReturn(time() + 3600);
-		$resource = new FileResource(__DIR__.'/mock.file', $files);
+		$files->shouldReceive('lastModified')->times(3)->andReturn(time(), time() + 3600);
+
+		$resource = new FileResource(new SplFileInfo(__FILE__), $files);
+
 		$events = $resource->detectChanges();
 		$this->assertInstanceOf('JasonLewis\ResourceWatcher\Event', $event = array_pop($events));
 		$this->assertEquals(Event::RESOURCE_MODIFIED, $event->getCode());
 	}
+
 
 	public function testDetectingOfResourceDeleted()
 	{
@@ -48,10 +63,13 @@ class FileResourceTest extends PHPUnit_Framework_TestCase {
 		$files->shouldReceive('exists')->once()->andReturn(true);
 		$files->shouldReceive('exists')->once()->andReturn(false);
 		$files->shouldReceive('lastModified')->once()->andReturn(time());
-		$resource = new FileResource(__DIR__.'/mock.file', $files);
+
+		$resource = new FileResource(new SplFileInfo(__FILE__), $files);
+
 		$events = $resource->detectChanges();
 		$this->assertInstanceOf('JasonLewis\ResourceWatcher\Event', $event = array_pop($events));
 		$this->assertEquals(Event::RESOURCE_DELETED, $event->getCode());
 	}
+
 
 }
